abf10c9cdfae2c5ee7474fd513f56f4fbf229389
answer0424 <wonwon123123@naver.com>
2025-01-22 16:22:02 +0900

    feat: Certification과 TrainerProfile의 복합키 설정으로 인해 발생하는 중간 테이블 문제 해결

--- src/main/java/com/lec/spring/domain/training/domain/Certification.java
+++ src/main/java/com/lec/spring/domain/training/domain/Certification.java
@@ -13,13 +13,15 @@ import lombok.NoArgsConstructor;
 public class Certification {
 
     @EmbeddedId
-    private CertificationId Id;
+    private CertificationId id;
 
+    @MapsId("trainerProfileId") // 복합 키의 trainerProfileId와 매핑
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "TrainerProfile_id", insertable = false, updatable = false)
+    @JoinColumn(name = "TrainerProfile_id", nullable = false)
     private TrainerProfile trainerProfile;
 
     @Column(name = "credentials")
     private String credentials;
 }
+
 
--- src/main/java/com/lec/spring/domain/training/domain/TrainerProfile.java
+++ src/main/java/com/lec/spring/domain/training/domain/TrainerProfile.java
@@ -2,11 +2,12 @@ package com.lec.spring.domain.training.domain;
 
 import com.lec.spring.domain.base.domain.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -37,5 +38,14 @@ public class TrainerProfile {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private GrantStatus isAccess;
+
+    @OneToMany(mappedBy = "trainerProfile", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    @Builder.Default
+    private List<Certification> certificationList = new ArrayList<>();
+
+    public void addCertificationList(Certification... certificationList) {
+        Collections.addAll(this.certificationList, certificationList);
+    }
 }
 
