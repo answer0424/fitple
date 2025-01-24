b7eb1a5f69efb677a10d2fb73e39442f343c665b
Donghee Lee <dhleedev47@gmail.com>
2025-01-24 19:33:36 +0900

    Feat(): HBTI, User_Student dummy data

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,18 +1,279 @@
 INSERT INTO User (username, password, email, nickname, address, birth, profile_image, authority, created_at)
-VALUES
-    ('user1', 'password1', 'user1@example.com', 'Runner1', 'Seoul, Korea', '1990-01-01', '/img/profile1.png', 'ROLE_USER', NOW()),
-    ('user2', 'password2', 'user2@example.com', 'Runner2', 'Busan, Korea', '1991-02-02', '/img/profile2.png', 'ROLE_USER', NOW()),
-    ('user3', 'password3', 'user3@example.com', 'Sprinter3', 'Daegu, Korea', '1992-03-03', '/img/profile3.png', 'ROLE_USER', NOW()),
-    ('user4', 'password4', 'user4@example.com', 'Jumper4', 'Incheon, Korea', '1993-04-04', '/img/profile4.png', 'ROLE_USER', NOW()),
-    ('user5', 'password5', 'user5@example.com', 'Cyclist5', 'Daejeon, Korea', '1994-05-05', '/img/profile5.png', 'ROLE_USER', NOW()),
-    ('user6', 'password6', 'user6@example.com', 'Swimmer6', 'Gwangju, Korea', '1995-06-06', '/img/profile6.png', 'ROLE_USER', NOW()),
-    ('user7', 'password7', 'user7@example.com', 'Lifter7', 'Suwon, Korea', '1996-07-07', '/img/profile7.png', 'ROLE_USER', NOW()),
-    ('user8', 'password8', 'user8@example.com', 'Rowing8', 'Ulsan, Korea', '1997-08-08', '/img/profile8.png', 'ROLE_USER', NOW()),
-    ('user9', 'password9', 'user9@example.com', 'Skater9', 'Jeju, Korea', '1998-09-09', '/img/profile9.png', 'ROLE_USER', NOW()),
-    ('user10', 'password10', 'user10@example.com', 'Hiker10', 'Gangwon, Korea', '1999-10-10', '/img/profile10.png', 'ROLE_USER', NOW());
+VALUES ('user1', 'password1', 'user1@example.com', 'Runner1', '서울특별시 강남구 대치동 888', '1990-01-01', '/img/profile1.png',
+        'ROLE_STUDENT', NOW()),
+       ('user2', 'password2', 'user2@example.com', 'Runner2', '서울특별시 강남구 삼성동 123', '1991-02-02', '/img/profile2.png',
+        'ROLE_STUDENT', NOW()),
+       ('user3', 'password3', 'user3@example.com', 'Sprinter3', '서울특별시 서초구 반포동 456', '1992-03-03', '/img/profile3.png',
+        'ROLE_STUDENT', NOW()),
+       ('user4', 'password4', 'user4@example.com', 'Jumper4', '서울특별시 서초구 서초동 789', '1993-04-04', '/img/profile4.png',
+        'ROLE_STUDENT', NOW()),
+       ('user5', 'password5', 'user5@example.com', 'Cyclist5', '서울특별시 송파구 잠실동 234', '1994-05-05', '/img/profile5.png',
+        'ROLE_STUDENT', NOW()),
+       ('user6', 'password6', 'user6@example.com', 'Swimmer6', '서울특별시 송파구 방이동 567', '1995-06-06', '/img/profile6.png',
+        'ROLE_STUDENT', NOW()),
+       ('user7', 'password7', 'user7@example.com', 'Lifter7', '서울특별시 강동구 천호동 890', '1996-07-07', '/img/profile7.png',
+        'ROLE_STUDENT', NOW()),
+       ('user8', 'password8', 'user8@example.com', 'Rowing8', '서울특별시 강동구 길동 123', '1997-08-08', '/img/profile8.png',
+        'ROLE_STUDENT', NOW()),
+       ('user9', 'password9', 'user9@example.com', 'Skater9', '서울특별시 용산구 이태원동 456', '1998-09-09', '/img/profile9.png',
+        'ROLE_STUDENT', NOW()),
+       ('user10', 'password10', 'user10@example.com', 'Hiker10', '서울특별시 용산구 한남동 789', '1999-10-10',
+        '/img/profile10.png', 'ROLE_STUDENT', NOW()),
+       ('user11', 'password11', 'user11@example.com', 'Boxer11', '서울특별시 마포구 합정동 234', '1990-11-11',
+        '/img/profile11.png', 'ROLE_STUDENT', NOW()),
+       ('user12', 'password12', 'user12@example.com', 'Fighter12', '서울특별시 마포구 상암동 567', '1991-12-12',
+        '/img/profile12.png', 'ROLE_STUDENT', NOW()),
+       ('user13', 'password13', 'user13@example.com', 'Athlete13', '서울특별시 성동구 성수동 890', '1992-01-13',
+        '/img/profile13.png', 'ROLE_STUDENT', NOW()),
+       ('user14', 'password14', 'user14@example.com', 'Player14', '서울특별시 성동구 금호동 123', '1993-02-14',
+        '/img/profile14.png', 'ROLE_STUDENT', NOW()),
+       ('user15', 'password15', 'user15@example.com', 'Racer15', '서울특별시 광진구 자양동 456', '1994-03-15',
+        '/img/profile15.png', 'ROLE_STUDENT', NOW()),
+       ('user16', 'password16', 'user16@example.com', 'Climber16', '서울특별시 광진구 구의동 789', '1995-04-16',
+        '/img/profile16.png', 'ROLE_STUDENT', NOW()),
+       ('user17', 'password17', 'user17@example.com', 'Dancer17', '서울특별시 동대문구 청량리동 234', '1996-05-17',
+        '/img/profile17.png', 'ROLE_STUDENT', NOW()),
+       ('user18', 'password18', 'user18@example.com', 'Gymnast18', '서울특별시 동대문구 이문동 567', '1997-06-18',
+        '/img/profile18.png', 'ROLE_STUDENT', NOW()),
+       ('user19', 'password19', 'user19@example.com', 'Wrestler19', '서울특별시 중랑구 면목동 890', '1998-07-19',
+        '/img/profile19.png', 'ROLE_STUDENT', NOW()),
+       ('user20', 'password20', 'user20@example.com', 'Trainer20', '서울특별시 중랑구 망우동 123', '1999-08-20',
+        '/img/profile20.png', 'ROLE_STUDENT', NOW()),
+       ('user21', 'password21', 'user21@example.com', 'Coach21', '서울특별시 성북구 길음동 456', '1990-09-21',
+        '/img/profile21.png', 'ROLE_STUDENT', NOW()),
+       ('user22', 'password22', 'user22@example.com', 'Mentor22', '서울특별시 성북구 정릉동 789', '1991-10-22',
+        '/img/profile22.png', 'ROLE_STUDENT', NOW()),
+       ('user23', 'password23', 'user23@example.com', 'Expert23', '서울특별시 강북구 수유동 234', '1992-11-23',
+        '/img/profile23.png', 'ROLE_STUDENT', NOW()),
+       ('user24', 'password24', 'user24@example.com', 'Pro24', '서울특별시 강북구 미아동 567', '1993-12-24', '/img/profile24.png',
+        'ROLE_STUDENT', NOW()),
+       ('user25', 'password25', 'user25@example.com', 'Master25', '서울특별시 도봉구 쌍문동 890', '1994-01-25',
+        '/img/profile25.png', 'ROLE_STUDENT', NOW()),
+       ('user26', 'password26', 'user26@example.com', 'Champion26', '서울특별시 도봉구 방학동 123', '1995-02-26',
+        '/img/profile26.png', 'ROLE_STUDENT', NOW()),
+       ('user27', 'password27', 'user27@example.com', 'Winner27', '서울특별시 노원구 상계동 456', '1996-03-27',
+        '/img/profile27.png', 'ROLE_STUDENT', NOW()),
+       ('user28', 'password28', 'user28@example.com', 'Victor28', '서울특별시 노원구 중계동 789', '1997-04-28',
+        '/img/profile28.png', 'ROLE_STUDENT', NOW()),
+       ('user29', 'password29', 'user29@example.com', 'Star29', '서울특별시 은평구 불광동 234', '1998-05-29', '/img/profile29.png',
+        'ROLE_STUDENT', NOW()),
+       ('user30', 'password30', 'user30@example.com', 'Legend30', '서울특별시 은평구 응암동 567', '1999-06-30',
+        '/img/profile30.png', 'ROLE_STUDENT', NOW()),
+       ('user31', 'password31', 'user31@example.com', 'Hero31', '서울특별시 서대문구 신촌동 890', '1990-07-31',
+        '/img/profile31.png', 'ROLE_STUDENT', NOW()),
+       ('user32', 'password32', 'user32@example.com', 'Idol32', '서울특별시 서대문구 연희동 123', '1991-08-01',
+        '/img/profile32.png', 'ROLE_STUDENT', NOW()),
+       ('user33', 'password33', 'user33@example.com', 'Icon33', '서울특별시 마포구 서교동 456', '1992-09-02', '/img/profile33.png',
+        'ROLE_STUDENT', NOW()),
+       ('user34', 'password34', 'user34@example.com', 'Star34', '서울특별시 마포구 망원동 789', '1993-10-03', '/img/profile34.png',
+        'ROLE_STUDENT', NOW()),
+       ('user35', 'password35', 'user35@example.com', 'Ace35', '서울특별시 양천구 목동 234', '1994-11-04', '/img/profile35.png',
+        'ROLE_STUDENT', NOW()),
+       ('user36', 'password36', 'user36@example.com', 'King36', '서울특별시 양천구 신정동 567', '1995-12-05', '/img/profile36.png',
+        'ROLE_STUDENT', NOW()),
+       ('user37', 'password37', 'user37@example.com', 'Queen37', '서울특별시 강서구 화곡동 890', '1996-01-06',
+        '/img/profile37.png', 'ROLE_STUDENT', NOW()),
+       ('user38', 'password38', 'user38@example.com', 'Knight38', '서울특별시 강서구 등촌동 123', '1997-02-07',
+        '/img/profile38.png', 'ROLE_STUDENT', NOW()),
+       ('user39', 'password39', 'user39@example.com', 'Bishop39', '서울특별시 구로구 구로동 456', '1998-03-08',
+        '/img/profile39.png', 'ROLE_STUDENT', NOW()),
+       ('user40', 'password40', 'user40@example.com', 'Rook40', '서울특별시 구로구 개봉동 789', '1999-04-09', '/img/profile40.png',
+        'ROLE_STUDENT', NOW()),
+       ('user41', 'password41', 'user41@example.com', 'Pawn41', '서울특별시 금천구 가산동 234', '1990-05-10', '/img/profile41.png',
+        'ROLE_STUDENT', NOW()),
+       ('user42', 'password42', 'user42@example.com', 'Guard42', '서울특별시 금천구 독산동 567', '1991-06-11',
+        '/img/profile42.png', 'ROLE_STUDENT', NOW()),
+       ('user43', 'password43', 'user43@example.com', 'Scout43', '서울특별시 영등포구 여의도동 890', '1992-07-12',
+        '/img/profile43.png', 'ROLE_STUDENT', NOW()),
+       ('user44', 'password44', 'user44@example.com', 'Ranger44', '서울특별시 영등포구 당산동 123', '1993-08-13',
+        '/img/profile44.png', 'ROLE_STUDENT', NOW()),
+       ('user45', 'password45', 'user45@example.com', 'Hunter45', '서울특별시 동작구 사당동 456', '1994-09-14',
+        '/img/profile45.png', 'ROLE_STUDENT', NOW()),
+       ('user46', 'password46', 'user46@example.com', 'Archer46', '서울특별시 동작구 상도동 789', '1995-10-15',
+        '/img/profile46.png', 'ROLE_STUDENT', NOW()),
+       ('user47', 'password47', 'user47@example.com', 'Warrior47', '서울특별시 관악구 봉천동 234', '1996-11-16',
+        '/img/profile47.png', 'ROLE_STUDENT', NOW()),
+       ('user48', 'password48', 'user48@example.com', 'Knight48', '서울특별시 관악구 신림동 567', '1997-12-17',
+        '/img/profile48.png', 'ROLE_STUDENT', NOW()),
+       ('user49', 'password49', 'user49@example.com', 'Samurai49', '서울특별시 종로구 평창동 890', '1998-01-18',
+        '/img/profile49.png', 'ROLE_STUDENT', NOW()),
+       ('user50', 'password50', 'user50@example.com', 'Ninja50', '서울특별시 종로구 삼청동 123', '1999-02-19',
+        '/img/profile50.png', 'ROLE_STUDENT', NOW());
 
-SELECT * FROM User WHERE username = 'user1';
-SELECT * FROM User;
-SELECT * FROM HBTI;
-SELECT * FROM HBTI WHERE user_id = '1';
+-- First 50 users (1-50)
+INSERT INTO HBTI (UserId, HBTI, 1col, 2col, 3col, 4col)
+VALUES
+-- MICP (3)
+(1, 'MICP', 70, 20, 58, 90),
+(2, 'MICP', 65, 25, 60, 85),
+(3, 'MICP', 75, 30, 55, 80),
+-- MECP (3)
+(4, 'MECP', 73, 85, 59, 89),
+(5, 'MECP', 68, 82, 62, 83),
+(6, 'MECP', 71, 88, 58, 87),
+-- BICP (3)
+(7, 'BICP', 30, 25, 58, 85),
+(8, 'BICP', 35, 28, 60, 82),
+(9, 'BICP', 28, 22, 62, 88),
+-- BECP (3)
+(10, 'BECP', 35, 82, 57, 86),
+(11, 'BECP', 32, 85, 60, 83),
+(12, 'BECP', 28, 88, 58, 87),
+-- MICG (3)
+(13, 'MICG', 72, 28, 58, 25),
+(14, 'MICG', 68, 25, 60, 28),
+(15, 'MICG', 75, 22, 57, 22),
+-- MECG (3)
+(16, 'MECG', 70, 85, 58, 30),
+(17, 'MECG', 75, 82, 60, 25),
+(18, 'MECG', 68, 88, 57, 28),
+-- BICG (3)
+(19, 'BICG', 35, 28, 58, 30),
+(20, 'BICG', 32, 25, 60, 25),
+(21, 'BICG', 28, 22, 62, 28),
+-- BECG (3)
+(22, 'BECG', 35, 82, 58, 30),
+(23, 'BECG', 32, 85, 60, 25),
+(24, 'BECG', 28, 88, 62, 28),
+-- MINP (3)
+(25, 'MINP', 72, 28, 30, 85),
+(26, 'MINP', 68, 25, 35, 82),
+(27, 'MINP', 75, 22, 32, 88),
+-- MENP (3)
+(28, 'MENP', 70, 85, 30, 85),
+(29, 'MENP', 75, 82, 35, 82),
+(30, 'MENP', 68, 88, 32, 88),
+-- BINP (3)
+(31, 'BINP', 35, 28, 30, 85),
+(32, 'BINP', 32, 25, 35, 82),
+(33, 'BINP', 28, 22, 32, 88),
+-- BENP (3)
+(34, 'BENP', 35, 82, 30, 85),
+(35, 'BENP', 32, 85, 35, 82),
+(36, 'BENP', 28, 88, 32, 88),
+-- MING (4)
+(37, 'MING', 72, 28, 30, 30),
+(38, 'MING', 68, 25, 35, 25),
+(39, 'MING', 75, 22, 32, 28),
+(40, 'MING', 70, 27, 33, 32),
+-- MENG (4)
+(41, 'MENG', 72, 82, 30, 30),
+(42, 'MENG', 68, 85, 35, 25),
+(43, 'MENG', 75, 88, 32, 28),
+(44, 'MENG', 70, 87, 33, 32),
+-- BING (4)
+(45, 'BING', 32, 28, 30, 30),
+(46, 'BING', 35, 25, 35, 25),
+(47, 'BING', 28, 22, 32, 28),
+(48, 'BING', 30, 27, 33, 32),
+-- BENG (3)
+(49, 'BENG', 32, 82, 30, 30),
+(50, 'BENG', 35, 85, 35, 25),
+-- MICP (4)
+(51, 'MICP', 72, 28, 58, 89),
+(52, 'MICP', 69, 25, 62, 86),
+(53, 'MICP', 75, 22, 59, 88),
+(54, 'MICP', 71, 27, 61, 85),
+-- MECP (4)
+(55, 'MECP', 73, 86, 57, 87),
+(56, 'MECP', 70, 83, 60, 85),
+(57, 'MECP', 76, 88, 58, 89),
+(58, 'MECP', 72, 85, 61, 86),
+-- BICP (4)
+(59, 'BICP', 32, 27, 59, 87),
+(60, 'BICP', 35, 24, 62, 85),
+(61, 'BICP', 29, 26, 58, 88),
+(62, 'BICP', 33, 23, 60, 86),
+-- BECP (4)
+(63, 'BECP', 31, 87, 57, 89),
+(64, 'BECP', 34, 84, 61, 85),
+(65, 'BECP', 30, 86, 59, 87),
+(66, 'BECP', 32, 83, 62, 86),
+-- MICG (4)
+(67, 'MICG', 74, 26, 58, 32),
+(68, 'MICG', 71, 23, 61, 28),
+(69, 'MICG', 76, 25, 57, 30),
+(70, 'MICG', 73, 28, 60, 25),
+-- MECG (4)
+(71, 'MECG', 75, 86, 59, 31),
+(72, 'MECG', 72, 83, 62, 28),
+(73, 'MECG', 77, 87, 58, 30),
+(74, 'MECG', 74, 84, 60, 27),
+-- BICG (4)
+(75, 'BICG', 33, 26, 57, 31),
+(76, 'BICG', 30, 23, 60, 28),
+(77, 'BICG', 35, 25, 58, 30),
+(78, 'BICG', 32, 28, 61, 27),
+-- BECG (4)
+(79, 'BECG', 34, 86, 59, 32),
+(80, 'BECG', 31, 83, 62, 29),
+(81, 'BECG', 36, 87, 58, 31),
+(82, 'BECG', 33, 84, 60, 28),
+-- MINP (4)
+(83, 'MINP', 73, 26, 32, 88),
+(84, 'MINP', 70, 23, 35, 85),
+(85, 'MINP', 76, 25, 31, 87),
+(86, 'MINP', 72, 28, 33, 86),
+-- MENP (4)
+(87, 'MENP', 74, 86, 33, 89),
+(88, 'MENP', 71, 83, 36, 86),
+(89, 'MENP', 77, 87, 32, 88),
+(90, 'MENP', 73, 84, 34, 85),
+-- BINP (4)
+(91, 'BINP', 32, 26, 33, 87),
+(92, 'BINP', 35, 23, 36, 84),
+(93, 'BINP', 29, 25, 32, 86),
+(94, 'BINP', 33, 28, 34, 83),
+-- BENP (4)
+(95, 'BENP', 31, 86, 32, 88),
+(96, 'BENP', 34, 83, 35, 85),
+(97, 'BENP', 30, 87, 31, 87),
+(98, 'BENP', 32, 84, 33, 84),
+-- MING (2 of 4)
+(99, 'MING', 74, 26, 32, 32),
+(100, 'MING', 71, 23, 35, 29),
+-- MING (2 remaining)
+(101, 'MING', 73, 25, 31, 28),
+(102, 'MING', 75, 28, 33, 31),
+-- MENG (4)
+(103, 'MENG', 76, 85, 32, 32),
+(104, 'MENG', 73, 82, 35, 29),
+(105, 'MENG', 78, 88, 31, 28),
+(106, 'MENG', 75, 85, 33, 31),
+-- BING (4)
+(107, 'BING', 32, 25, 32, 32),
+(108, 'BING', 35, 22, 35, 29),
+(109, 'BING', 29, 28, 31, 28),
+(110, 'BING', 33, 25, 33, 31),
+-- BENG (5)
+(111, 'BENG', 31, 85, 32, 32),
+(112, 'BENG', 34, 82, 35, 29),
+(113, 'BENG', 30, 88, 31, 28),
+(114, 'BENG', 32, 85, 33, 31),
+(115, 'BENG', 33, 87, 34, 30),
+-- Rest distributed among various types for balance
+(116, 'MICP', 72, 25, 58, 88),
+(117, 'MECP', 74, 87, 59, 85),
+(118, 'BICP', 31, 25, 57, 86),
+(119, 'BECP', 33, 85, 58, 87),
+(120, 'MICG', 75, 25, 59, 30),
+(121, 'MECG', 76, 86, 58, 29),
+(122, 'BICG', 34, 25, 57, 31),
+(123, 'BECG', 32, 87, 58, 30),
+(124, 'MINP', 74, 25, 32, 85),
+(125, 'MENP', 75, 86, 33, 86)
+;
+
+SELECT *
+FROM User
+WHERE username = 'user1';
+SELECT *
+FROM User;
+SELECT *
+FROM User;
+SELECT *
+FROM HBTI;
+SELECT *
+FROM HBTI
+WHERE user_id = '1';
 
