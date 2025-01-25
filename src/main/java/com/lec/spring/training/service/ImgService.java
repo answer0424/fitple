645aa028857abcf5822155dbabfc816c73d61a0c
Xuswnss <cooconut0226@gmail.com>
2025-01-25 18:28:40 +0900

    Feat (#8) : TrainerDetailService/ TrainerDetailServiceImple + ImgService, ImgServiceImpl 추가 + 주석

--- src/main/java/com/lec/spring/training/service/ImgService.java
+++ src/main/java/com/lec/spring/training/service/ImgService.java
@@ -8,9 +8,13 @@ import java.util.List;
 
 public interface ImgService {
 
+    // 여러이미지 파일 저장 ->  List
     void saveImages(List<MultipartFile> files, String dir) throws IOException;
+    // 단일 이미지 저장 -> MulitpartFile (필요한가?)
     Path saveImage(MultipartFile file, String dir) throws IOException;
+    // img 불러오기
     byte[] loadImage(String filename, String dir) throws IOException;
+    // 삭제
     void deleteImage(String filename, String dir) throws IOException;
 
 }
