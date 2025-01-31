2fd42bcfa4281ed1af6249c72e170421f38ef32d
kp-of-s <gimbhs@gmail.com>
2025-01-31 18:39:47 +0900

    feat(#17) : CertificationId AI 설정

--- src/main/java/com/lec/spring/training/domain/CertificationId.java
+++ src/main/java/com/lec/spring/training/domain/CertificationId.java
@@ -2,6 +2,8 @@ package com.lec.spring.training.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -17,6 +19,7 @@ import java.io.Serializable;
 public class CertificationId implements Serializable {
 
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "trainerProfileId")
